c92fc43c91af86a16ab6fa6fac17655a786dfad6
RafalBie <homefac@gmail.com>
2020-04-27 14:08:44 +0200

    commit

--- week.js
+++ week.js
@@ -8,4 +8,25 @@
 // - przeszłość ma być zaznaczona na ciemnoszary kolor fontu
 // - przyszłość tydzień w przód kolor fontu ma mieć jasno zielony
 // - nad kalend
-moment().format('MMMM Do YYYY, h:mm:ss a');
+// moment().format('MMMM Do YYYY, h:mm:ss a');
+// moment().subtract(10, 'days').calendar();
+// moment().subtract(10, 'days').calendar();
+// console.log (moment().calendar())
+// console.log(moment().locale())
+// console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+window.onload = calendar;
+
+let today = moment().format('dddd');
+let locate = moment.locale();
+   
+
+
+const tbl = document.getElementById("cell");
+const newDay = document.createElement('th')
+newDay.innerHTML = today
+tbl.appendChild(newDay)
+
+
+
+
