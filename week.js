9394c35462f2931534122cefca4ac4efb83a813f
RafalBie <homefac@gmail.com>
2020-04-28 20:06:30 +0200

    commit

--- week.js
+++ week.js
@@ -7,7 +7,8 @@
 // - dzisiejszy dzień ma być zaznaczony na zielono kolor fontu
 // - przeszłość ma być zaznaczona na ciemnoszary kolor fontu
 // - przyszłość tydzień w przód kolor fontu ma mieć jasno zielony
-// - nad kalend
+// - nad kalendarzem w górze ma się wyświetlać godzina zgodna z lokalizacją
+
 // moment().format('MMMM Do YYYY, h:mm:ss a');
 // moment().subtract(10, 'days').calendar();
 // moment().subtract(10, 'days').calendar();
@@ -15,17 +16,20 @@
 // console.log(moment().locale())
 // console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
-window.onload = calendar;
+// window.onload = calendar;
+window.onload = function calendar(){
+    
+        let today = moment().format('dddd');
+        let now = moment().format('DD.MM.YYYY');
+        let locate = moment.locale();
+        const tbl = document.getElementById("cell");
+        const newDay = document.createElement('th');
+        newDay.innerHTML = now;
+        tbl.appendChild(newDay)
 
-let today = moment().format('dddd');
-let locate = moment.locale();
-   
+    
+    }
 
-
-const tbl = document.getElementById("cell");
-const newDay = document.createElement('th')
-newDay.innerHTML = today
-tbl.appendChild(newDay)
 
 
 
