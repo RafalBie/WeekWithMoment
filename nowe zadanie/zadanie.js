08d7ef871a48de00d511d32bed530cc2ff62afe0
RafalBie <homefac@gmail.com>
2020-06-17 17:25:00 +0200

    commit

--- nowe zadanie/zadanie.js
+++ nowe zadanie/zadanie.js
@@ -6,15 +6,28 @@
 
 // - progress bar powienien być sticky do górnej krawędzi ekranu w obrębie elementu z klasą .with-progress-reading-bar / jeśli przecina się z górną krawędzią okna
 
+// jeśli dany element o klasie .with-progress-reading-bar ma wysokość 500px
+
 function addReadingProgressBars() {
   // znaleźć elementy z klasą .with-progress-reading-bar
-  const winScroll = document.documentElement.scrollTop;
+  const winScroll = document.documentElement.scrollTop; // window.scrollTop
   console.log(winScroll);
-  const height = document.documentElement.scrollHeight;
+  // const height = document.documentElement.scrollHeight;
+  const height = document.querySelector(".content1").scrollHeight;
   console.log(height);
-  const scrolled = (winScroll / height) * 100;
-  console.log(scrolled);
-  document.getElementById("myBar").style.width = scrolled + "%";
+  const height2 = document.querySelector(".content2").scrollHeight;
+  console.log(height2);
+  if (winScroll < height) {
+    const scrolled = (winScroll / height) * 100;
+    // console.log(scrolled);
+
+    document.getElementById("myBar").style.width = scrolled + "%";
+  } else if (winScroll > height) {
+    const scrolled2 = (winScroll / height2) * 100;
+    // console.log(scrolled);
+
+    document.getElementById("myBar2").style.width = scrolled2 + "%";
+  }
 
   // dla każdego z tych elemementów odzyskać jego pozycje i wymiary
 }
